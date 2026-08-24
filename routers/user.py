f5b8c36dd697443ee2bf1d90349b6567295723f6
wutangxuelili <3214710338@qq.com>
2026-08-24 16:28:40 +0800

    xiugai

--- routers/user.py
+++ routers/user.py
@@ -14,7 +14,7 @@ from schemas.user import UserCreate
 load_dotenv()
 router = APIRouter()
 
-SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key-123")
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
 
